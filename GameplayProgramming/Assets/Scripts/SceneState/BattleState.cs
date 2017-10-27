ba55fc507825c86fbb411355b9fdd88f6f748784
Chuan-Chin Lai <chuanchinlai@gmail.com>
2017-10-27 13:12:34 -0600

    first commit

---
+++ GameplayProgramming/Assets/Scripts/SceneState/BattleState.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class BattleState : ISceneState
+{
+	public BattleState(SceneStateController Controller) : base(Controller)
+	{
+		this.StateName = "BattleState";
+	}
+
+
+	public override void StateBegin()
+	{
+        TowerDefenseGame.Instance.Initinal();
+	}
+
+
+	public override void StateEnd()
+	{
+        TowerDefenseGame.Instance.Release();
+	}
+			
+
+	public override void StateUpdate()
+	{
+
+        TowerDefenseGame.Instance.Update();
+
+		if(TowerDefenseGame.Instance.ThisGameIsOver())
+			m_Controller.SetState(new MainMenuState(m_Controller), "MainMenuScene" );
+	}
+}
---
+++ GameplayProgramming/Assets/Scripts/SceneState/SceneStateController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class SceneStateController
+{
+	private ISceneState m_State;
+	private bool 	m_bRunBegin = false;
+	
+	public SceneStateController()
+	{}
+
+
+	public void SetState(ISceneState State, string LoadSceneName)
+	{
+		//Debug.Log ("SetState:"+State.ToString());
+		m_bRunBegin = false;
+
+		LoadScene( LoadSceneName );
+
+		if( m_State != null )
+			m_State.StateEnd();
+
+		m_State = State;
+	}
+
+
+	private void LoadScene(string LoadSceneName)
+	{
+		if( LoadSceneName == null || LoadSceneName.Length == 0 )
+			return ;
+
+        SceneManager.LoadScene(LoadSceneName);
+//		Application.LoadLevel( LoadSceneName );
+	}
+
+
+	public void StateUpdate()
+	{
+ //       AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(m_State.StateName);
+
+		if (Application.isLoadingLevel)
+			return;
+
+		// call state init()
+		if( m_State != null && m_bRunBegin == false)
+		{
+			m_State.StateBegin();
+			m_bRunBegin = true;
+		}
+
+        //call state update()
+		if( m_State != null)
+			m_State.StateUpdate();
+	}
+}
